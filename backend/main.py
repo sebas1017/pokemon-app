b1f8ba9e070cbe8e025d728eb73672c597ec09ba
sebas1017 <henaoerazosebastian995@gmail.com>
2022-05-02 22:03:17 -0500

    control de origenes

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 app.add_middleware(
 
     CORSMiddleware,
-    allow_origins=["*","http://website-pokemon-cards:3000"], 
+    allow_origins=["http://website-pokemon-cards:3000"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
